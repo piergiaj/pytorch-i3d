2e4191f8a9c035b2ec555f2411b70b56975f67cd
Richard Hsieh <rhsieh@DN51v4b9.SUNet>
2019-08-09 10:06:52 -0700

    Fixed file loading path issues.

--- dataset_torchvision.py
+++ dataset_torchvision.py
@@ -1,9 +1,13 @@
+import os
 from torchvision.datasets.ucf101 import UCF101
 from torch.utils.data import DataLoader
 
-def load_ucf101():
-    d = UCF101(root='/Users/rhsieh/Desktop/Stanford/CV_Research/ucf101/data',
-               annotation_path='/Users/rhsieh/Desktop/Stanford/CV_Research/ucf101/ucfTrainTestlist',
+def load_ucf101(root='data/ucf101/video', annotation_path='data/ucf101/ucfTrainTestlist'):
+    root = os.path.join(os.getcwd(), root)
+    annotation_path = os.path.join(os.getcwd(), annotation_path)
+
+    d = UCF101(root=root,
+               annotation_path=annotation_path,
                frames_per_clip=16,
                step_between_clips=16,
                fold=1,
@@ -11,8 +15,9 @@ def load_ucf101():
 
     return d
 
+
 if __name__ == '__main__':
-    d = load_ucf()
+    d = load_ucf101()
     train_loader = DataLoader(d)
 
     print(len(train_loader))
