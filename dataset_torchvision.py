b0af1ff07cc9164aa917e95727bddf549638d22f
Richard Hsieh <rhsieh@DN0a224836.SUNet>
2019-08-09 15:13:37 -0700

    Updated file path handling.

--- dataset_torchvision.py
+++ dataset_torchvision.py
@@ -2,10 +2,8 @@ import os
 from torchvision.datasets.ucf101 import UCF101
 from torch.utils.data import DataLoader
 
-def load_ucf101(root='data/ucf101/video', annotation_path='data/ucf101/ucfTrainTestlist'):
-    root = os.path.join(os.getcwd(), root)
-    annotation_path = os.path.join(os.getcwd(), annotation_path)
 
+def load_ucf101(root, annotation_path):
     d = UCF101(root=root,
                annotation_path=annotation_path,
                frames_per_clip=16,
@@ -17,7 +15,10 @@ def load_ucf101(root='data/ucf101/video', annotation_path='data/ucf101/ucfTrainT
 
 
 if __name__ == '__main__':
-    d = load_ucf101()
+    root = os.path.join(os.getcwd(), 'data/ucf101/clips')
+    annotation_path = os.path.join(os.getcwd(), 'data/ucf101/ucfTrainTestlist')
+
+    d = load_ucf101(root, annotation_path)
     train_loader = DataLoader(d)
 
     print(len(train_loader))
