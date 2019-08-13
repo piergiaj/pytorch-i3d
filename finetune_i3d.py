a350ea8dc164b41340626dbb26d85239f7ca529c
Richard Hsieh <rhsieh@DN0a224326.SUNet>
2019-08-13 11:34:51 -0700

    Tidied up training and data loading functions to make training loop more general.

--- finetune_i3d.py
+++ finetune_i3d.py
@@ -2,73 +2,31 @@ import os
 import torch
 import torchvision
 import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.transforms as T
 import torch.optim as optim
-import videotransforms
+import torch.nn.functional as F
 
-from torch.utils.data import DataLoader
-from torch.optim import lr_scheduler
-from torch.autograd import Variable
-# from dataset import TSNDataSet
+from videotransforms import *
+from dataset_torchvision import *
 from pytorch_i3d import InceptionI3d
-from transforms import Stack, ToTorchFormatTensor
-
-# newly released UCF101 dataset on pytorch=1.2.0 and torchvision=0.4
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader
 from torchvision.datasets.ucf101 import UCF101
 
 
-USE_GPU = True
-NUM_CLASSES = 101
-
-
-def train(fold=1, init_lr=0.1, max_steps=64e3, save_model='', use_gpu=False):
-    """
-    Load pretrained I3D model and finetune on new dataset.
-    """
+def train(model, optimizer, train_loader, test_loader, num_classes, max_steps, save_model='', use_gpu=False):
     # Enable GPU if available
     if USE_GPU and torch.cuda.is_available():
         device = torch.device('cuda')
     else:
         device = torch.device('cpu')
+
     print('Using device:', device)
+    i3d = i3d.to(device=device) # move model parameters to CPU/GPU
 
-    # Load data
-    root = os.path.join(os.getcwd(), 'data/ucf101/clips')
-    annotation_path = os.path.join(os.getcwd(), 'data/ucf101/ucfTrainTestlist')
+    dataloaders = {'train': train_loader, 'val': test_loader} 
 
-    training_set = UCF101(root=root,
-                          annotation_path=annotation_path,
-                          frames_per_clip=16,
-                          step_between_clips=16,
-                          fold=fold,
-                          train=True,
-                          transform=T.Compose([videotransforms.RandomCrop(224)]))
-    test_set = UCF101(root=root,
-                      annotation_path=annotation_path,
-                      frames_per_clip=16,
-                      step_between_clips=16,
-                      fold=fold,
-                      train=False,
-                      transform=T.Compose([videotransforms.RandomCrop(224)]))
-
-    loader_train = DataLoader(training_set)
-    loader_test = DataLoader(test_set)
-    dataloaders = {'train': loader_train, 'val': loader_test}
-
-    # Load model
-    i3d = InceptionI3d(400, in_channels=3)
-    i3d.load_state_dict(torch.load('models/rgb_imagenet.pt'))
-    i3d.replace_logits(NUM_CLASSES) # replace final layer to work with new dataset
-    i3d = i3d.to(device=device) # move the model parameters to CPU/GPU
-
-    # Define optimizer
-    lr = init_lr
-    optimizer = optim.SGD(i3d.parameters(), lr=lr, momentum=0.9, weight_decay=0.0000001)
-    lr_sched = optim.lr_scheduler.MultiStepLR(optimizer, [300, 1000])
-
-    # Start training
-    num_steps_per_update = 4 # accum gradient
+    # Training loop
+    num_steps_per_update = 4 # back-propagate every 4 steps
     steps = 0
     while steps < max_steps: # for epoch in range(num_epochs):
         print('Step {}/{}'.format(steps, max_steps))
@@ -77,9 +35,9 @@ def train(fold=1, init_lr=0.1, max_steps=64e3, save_model='', use_gpu=False):
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
             if phase == 'train':
-                i3d.train(True)
+                model.train(True)
             else:
-                i3d.train(False)  # set model to eval mode
+                model.train(False)  # set model to eval mode
                 
             tot_loss = 0.0
             tot_loc_loss = 0.0
@@ -87,20 +45,20 @@ def train(fold=1, init_lr=0.1, max_steps=64e3, save_model='', use_gpu=False):
             num_iter = 0
             optimizer.zero_grad()
             
-            # Iterate over data.
+            # Iterate over data
             for data in dataloaders[phase]:
                 num_iter += 1
                 inputs, _, class_idx = data # 2nd element of data tuple is audio
-                inputs = inputs.permute(0, 4, 1, 2, 3) # Swap from BTHWC to BCTHW
+                inputs = inputs.permute(0, 4, 1, 2, 3) # swap from BxTxHxWxC to BxCxTxHxW
                 inputs = inputs.to(device=device, dtype=torch.float32) # model expects inputs of float32
-                labels = torch.zeros([NUM_CLASSES]) # create one-hot tensor for label
+                labels = torch.zeros([num_classes]) # create one-hot tensor for label
                 labels[class_idx] = 1 
-                labels = labels.view(1, NUM_CLASSES, 1) # reshape to match model output
+                labels = labels.view(1, num_classes, 1) # reshape to match model output
                 labels = labels.to(device=device)
 
                 # Forward pass
                 t = inputs.shape[2]
-                per_frame_logits = i3d(inputs)
+                per_frame_logits = model(inputs)
                 print('per_frame_logits shape = {}'.format(per_frame_logits.shape))
                 per_frame_logits = F.interpolate(per_frame_logits, size=t, mode='linear') # upsample to match number of frames
 
@@ -112,20 +70,21 @@ def train(fold=1, init_lr=0.1, max_steps=64e3, save_model='', use_gpu=False):
                 cls_loss = F.binary_cross_entropy_with_logits(torch.max(per_frame_logits, dim=2)[0], torch.max(labels, dim=2)[0])
                 tot_cls_loss += cls_loss
 
-                loss = (0.5*loc_loss + 0.5*cls_loss)/num_steps_per_update
+                # Compute total loss and back-propagate
+                loss = (0.5*loc_loss + 0.5*cls_loss) / num_steps_per_update
                 tot_loss += loss
-                loss.backward()
+                loss.backward() 
 
                 if num_iter == num_steps_per_update and phase == 'train':
                     steps += 1
                     num_iter = 0
                     optimizer.step()
                     optimizer.zero_grad()
-                    lr_sched.step()
+                    # lr_sched.step()
+
                     if steps % 10 == 0:
                         print('{} Loc Loss: {:.4f} Cls Loss: {:.4f} Tot Loss: {:.4f}'.format(phase, tot_loc_loss/(10*num_steps_per_update), tot_cls_loss/(10*num_steps_per_update), tot_loss/10))
-                        # save model
-                        torch.save(i3d.module.state_dict(), save_model+str(steps).zfill(6)+'.pt')
+                        torch.save(model.state_dict(), save_model+str(steps).zfill(6)+'.pt')
                         tot_loss = tot_loc_loss = tot_cls_loss = 0.
 
             if phase == 'val':
@@ -133,4 +92,57 @@ def train(fold=1, init_lr=0.1, max_steps=64e3, save_model='', use_gpu=False):
 
 
 if __name__ == '__main__':
-    train(fold=1, use_gpu=True)
+    # Parameters
+    USE_GPU = True
+    NUM_CLASSES = 101 # number of classes in UCF101
+    FRAMES_PER_CLIP = 25 # UCF101 has a frame rate of 25 fps with a min clip length of 1.06 s
+    STEPS_BETWEEN_CLIPS = 25
+    FOLD = 1
+    BATCH_SIZE = 8
+
+    # Load dataset
+    root = os.path.join(os.getcwd(), 'data/ucf101/clips')
+    annotation_path = os.path.join(os.getcwd(), 'data/ucf101/ucfTrainTestlist')
+
+    train_transform = T.Compose([videotransforms.RandomCrop(224)]) # I3D model expects input HxW of 224x224
+    test_transform = T.Compose([videotransforms.CenterCrop(224)])
+    
+    d_train = UCF101(root,
+                     annotation_path,
+                     frames_per_clip=FRAMES_PER_CLIP,
+                     step_between_clips=STEPS_BETWEEN_CLIPS,
+                     fold=FOLD,
+                     train=True,
+                     transform=train_transform)
+    train_loader = DataLoader(d_train, 
+                              batch_size=BATCH_SIZE,
+                              shuffle=True, 
+                              num_workers=8,
+                              pin_memory=True)
+
+    d_test = UCF101(root,
+                    annotation_path,
+                    frames_per_clip=FRAMES_PER_CLIP,
+                    step_between_clips=STEPS_BETWEEN_CLIPS,
+                    fold=FOLD,
+                    train=False,
+                    transform=test_transform)
+    test_loader = DataLoader(d_test,
+                             batch_size=BATCH_SIZE,
+                             shuffle=True,
+                             num_workers=8,
+                             pin_memory=True)
+    
+    # Load pre-trained I3D model
+    i3d = InceptionI3d(400, in_channels=3) # pre-trained model has 400 output classes
+    i3d.load_state_dict(torch.load('models/rgb_imagenet.pt'))
+    i3d.replace_logits(NUM_CLASSES) # replace final layer to work with new dataset
+
+    # Set up optimizer
+    optimizer = optim.Adam(i3d.parameters(), lr=0.01)
+    # optimizer = optim.SGD(i3d.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0000001)
+    # lr_sched = optim.lr_scheduler.MultiStepLR(optimizer, [300, 1000])
+
+    # Start training
+    train(i3d, optimizer, train_loader, test_loader, 
+          max_steps=64e3, num_classes=NUM_CLASSES, use_gpu=USE_GPU)
