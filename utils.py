707086c9ebe0bafe1a994d71a750ffc408942211
Samuel Kwong <samkwong@stanford.edu>
2019-11-01 11:18:28 -0700

    Jester data loading

--- utils.py
+++ utils.py
@@ -1,60 +1,21 @@
-import csv
+import os
 
 
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-
-class Logger(object):
-
-    def __init__(self, path, header):
-        self.log_file = open(path, 'w')
-        self.logger = csv.writer(self.log_file, delimiter='\t')
-
-        self.logger.writerow(header)
-        self.header = header
-
-    def __del(self):
-        self.log_file.close()
-
-    def log(self, values):
-        write_values = []
-        for col in self.header:
-            assert col in values
-            write_values.append(values[col])
-
-        self.logger.writerow(write_values)
-        self.log_file.flush()
-
-
-def load_value_file(file_path):
-    with open(file_path, 'r') as input_file:
-        value = float(input_file.read().rstrip('\n\r'))
-
-    return value
-
-
-def calculate_accuracy(outputs, targets):
-    batch_size = targets.size(0)
-
-    _, pred = outputs.topk(1, 1, True)
-    pred = pred.t()
-    correct = pred.eq(targets.view(1, -1))
-    n_correct_elems = correct.float().sum().data[0]
-
-    return n_correct_elems / batch_size
+def save_images_for_debug(dir_img, imgs):
+    """
+    2x3x12x224x224 --> [BS, C, seq_len, H, W]
+    """
+    print("Saving images to {}".format(dir_img))
+    from matplotlib import pylab as plt
+    imgs = imgs.permute(0, 2, 3, 4, 1)  # [BS, seq_len, H, W, C]
+    imgs = imgs.mul(255).numpy()
+    if not os.path.exists(dir_img):
+        os.makedirs(dir_img)
+    print(imgs.shape)
+    for batch_id, batch in enumerate(imgs):
+        batch_dir = os.path.join(dir_img, "batch{}".format(batch_id + 1))
+        if not os.path.exists(batch_dir):
+            os.makedirs(batch_dir)
+        for j, img in enumerate(batch):
+            plt.imsave(os.path.join(batch_dir, "frame{%04d}.png" % (j + 1)),
+                       img.astype("uint8"))
