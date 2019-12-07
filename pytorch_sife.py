b3c591f5f86c66978961d90192513bfb955df369
Richard Hsieh <rhsieh@DN0a224986.SUNet>
2019-12-06 17:04:21 -0800

    Rewrote adversarial network implementation. Previous commit was not creating the network as intended.

--- pytorch_sife.py
+++ pytorch_sife.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torch.autograd import Function
-from pytorch_i3d import InceptionI3d
+from pytorch_i3d import *
 
 
 ## ReverseLayerF is copied from https://github.com/jindongwang/transferlearning/tree/master/code/deep/DANN(RevGrad)
@@ -11,55 +11,54 @@ from pytorch_i3d import InceptionI3d
 class ReverseLayerF(Function):
     """Reverse the gradients."""
     @staticmethod
-    def forward(ctx, x, alpha):
-        ctx.alpha = alpha
+    def forward(ctx, x):
 
         return x.view_as(x)
 
     @staticmethod
     def backward(ctx, grad_output):
-        output = grad_output.neg() * ctx.alpha
+        output = grad_output.neg()
 
         return output, None
 
 
-class ScenePredictor(nn.Module):
-    """Scene prediction branch. Inputs are features from the main network."""
-    def __init__(self, num_features, num_scene_classes):
-        super().__init__()
-
-        self.fc1 = nn.Linear(in_features=num_features, out_features=num_features)
-        self.bn1 = nn.BatchNorm1d(num_features=num_features)
-        self.fc2 = nn.Linear(in_features=num_features, out_features=num_features)
-        self.bn2 = nn.BatchNorm1d(num_features=num_features)
-        self.fc3 = nn.Linear(in_features=num_features, out_features=num_scene_classes)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = F.relu(self.bn1(x))
-        x = self.fc2(x)
-        x = F.relu(self.bn2(x))
-        x = self.fc3(x)
-
-        return x
-
-
-class SIFE(InceptionI3d):
+class SIFE(nn.Module):
     """Scene Invariant Feature Learning network."""
-    def __init__(self, num_action_classes=400, spatial_squeeze=True, final_endpoint='Logits', 
-                 name='SIFE_i3d', in_channels=3, dropout_keep_prob=0.5, 
-                 num_features, num_scene_classes):
-        super().__init__(num_action_classes=400, spatial_squeeze=True, final_endpoint='Logits', 
-                         name='SIFE_i3d', in_channels=3, dropout_keep_prob=0.5)
+    def __init__(self, backbone, num_features, num_actions, num_scenes, dropout_keep_prob=0.5):
+        super(SIFE, self).__init__()
+        # Backbone for feature extractor
+        self.backbone = backbone
 
-        self.Scene_Branch = ScenePredictor(num_features, num_scene_classes)
-        # TODO: might have to change variable 'name' from the base class
+        # Network for action classification
+        self.action_branch = nn.Sequential()
+        self.action_branch.add_module('a_dropout', nn.Dropout(dropout_keep_prob))
+        self.action_branch.add_module('a_unit3d', Unit3D(in_channels=384+384+128+128, 
+                                                         output_channels=num_actions,
+                                                         kernel_shape=[1, 1, 1],
+                                                         padding=0,
+                                                         activation_fn=None,
+                                                         use_batch_norm=False,
+                                                         use_bias=True,
+                                                         name='logits'))
+
+        # Network for scene classification
+        self.scene_branch = nn.Sequential()
+        self.scene_branch.add_module('s_fc1', nn.Linear(num_features, num_features))
+        self.scene_branch.add_module('s_bn1', nn.BatchNorm1d(num_features))
+        self.scene_branch.add_module('s_relu1', nn.ReLU(inplace=True))
+        self.scene_branch.add_module('s_fc2', nn.Linear(num_features, num_features))
+        self.scene_branch.add_module('s_bn2', nn.BatchNorm1d(num_features))
+        self.scene_branch.add_module('s_relu2', nn.ReLU(inplace=True))
+        self.scene_branch.add_module('s_fc3', nn.Linear(num_features, num_scenes))
 
     def forward(self, x):
-        # super() provides access to methods of the base class which have been overridden in the subclass 
-        action_logits = super().forward(x)
-        features = self.extract_features(x)
-        reversed_features = ReverseLayerF.apply(features)
-        scene_logits = self.Scene_Branch(reversed_features)
+        features = self.backbone.extract_features(x)
+        
+        action_logits = self.action_branch(features)
+        action_logits = action_logits.squeeze(3).squeeze(3) # original i3d model performs this so copying it here as well
+
+        reversed_features = ReverseLayerF.apply(features) # reverse the gradient
+        reversed_features = reversed_features.squeeze() # scene_branch expects 1D vectors
+        scene_logits = self.scene_branch(reversed_features)
 
         return action_logits, scene_logits
