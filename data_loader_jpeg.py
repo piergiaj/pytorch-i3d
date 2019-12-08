961f1a43ef6a3e140cadbc8b4d097659d7e67157
samkwong <samueljunkwong@gmail.com>
2019-12-07 22:48:20 -0800

    data_loader_jpeg changes

--- data_loader_jpeg.py
+++ data_loader_jpeg.py
@@ -45,7 +45,7 @@ class VideoFolder(torch.utils.data.Dataset):
             img = self.transform(img)
             imgs.append(torch.unsqueeze(img, 0))
 
-        target_idx = self.classes_dict[item.label] # TODO modify this
+        target_idx = self.classes_dict[item.label]
 
         # format data to torch
         data = torch.cat(imgs)
