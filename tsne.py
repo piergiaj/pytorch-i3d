a141e84a0ac1a3f37b3ccae67229d5326ecbdd16
rhsieh91 <rhsieh91@sc.stanford.edu>
2019-12-11 22:44:13 -0800

    Appending input features to a np array instead of list now.

--- tsne.py
+++ tsne.py
@@ -19,31 +19,32 @@ from data_loader_jpeg import *
 from sklearn.manifold import TSNE
 from collections import OrderedDict
 
-NUM_ACTIONS=5
-NUM_SCENES=2
+NUM_ACTIONS = 5
+NUM_SCENES = 2
+NUM_FEATURES = 1024
 NUM_WORKERS = 2
 BATCH_SIZE = 128
 SHUFFLE = False
 PIN_MEMORY = True
-ADVERSARIAL=False
-DATA_PARALLEL=True
+ADVERSARIAL = False
+DATA_PARALLEL = True
 
 i3d = InceptionI3d(400, in_channels=3)
 if not ADVERSARIAL:
     model = i3d
     model.replace_logits(NUM_ACTIONS)
 else:
-    model = SIFE(backbone=i3d, num_features=1024, num_actions=NUM_ACTIONS, num_scenes=SCENES)
+    model = SIFE(backbone=i3d, num_features=NUM_FEATURES, num_actions=NUM_ACTIONS, num_scenes=SCENES)
 
 print("Loading checkpoint...")
 if DATA_PARALLEL: # If training was run with nn.DataParallel, need extra steps before loading checkpoint
-    state_dict = torch.load('./checkpoints-2019-12-9-22-36-12/04018530.pt')['model_state_dict']
+    state_dict = torch.load('./checkpoints-2019-12-9-22-36-12/04018530.pt')['model_state_dict'] # baseline weights
     checkpoint = OrderedDict()
     for k, v in state_dict.items():
         name = k[7:] # remove 'module'
         checkpoint[name] = v
 else:
-     checkpoint = torch.load('./checkpoints-2019-12-9-15-47-16/02022233.pt')['model_state_dict']
+     checkpoint = torch.load('./checkpoints-2019-12-9-15-47-16/02022233.pt')['model_state_dict'] # adversarial weights
 model.load_state_dict(checkpoint)
 print("Loaded.")
 
@@ -73,17 +74,15 @@ train_loader = DataLoader(d_train,
                           num_workers=NUM_WORKERS,
                           pin_memory=PIN_MEMORY)
 
-# TypeError: can't convert CUDA tensor to numpy. Use Tensor.cpu() to copy the tensor to host memory first.
 if torch.cuda.is_available():
     device = torch.device('cuda')
 else:
     device = torch.device('cpu')
     print('Using device:', device)
-
 model = model.to(device=device) # move model parameters to CPU/GPU
 
-inputs_features = [] # to hold all inputs' feature arrays
 print('Starting feature extraction with batch size = {}'.format(BATCH_SIZE))
+inputs_features = np.empty((0, NUM_FEATURES)) # to hold all inputs' feature arrays
 for i,  data in enumerate(train_loader):
     print("Extracting features from batch {}".format(i))
     inputs = data[0]
@@ -94,11 +93,16 @@ for i,  data in enumerate(train_loader):
         else:
             features = model.backbone.extract_features(inputs)
     features = features.squeeze()
-    inputs_features.append(features)
+    features = features.cpu().detach().numpy()
+    inputs_features = np.append(inputs_features, features, axis=0) 
+
+print('inputs_features shape = {}'.format(inputs_features.shape))
+print('Saving features')
+np.save('inputs_features_baseline', inputs_features)
 
 print("Starting TSNE")
-features_embedded = TSNE(n_components=2).fit_transform(inputs_features.cpu().detach().numpy()) # might need >1024 features eventually (at least 8000)
-print("Finished TSNE.")
+features_embedded = TSNE(n_components=2).fit_transform(inputs_features)
+print("Finished TSNE")
 print('feautures_embedded shape = {}'.format(features_embdeed.shape))
 
 # TODO plot features_embedded
