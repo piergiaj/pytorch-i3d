a07eea8e4e94bf75307a34bd680dc8594b7cfbc4
samkwong <samueljunkwong@gmail.com>
2019-12-25 15:27:54 -0800

    Clean script

--- tsne.py
+++ tsne.py
@@ -21,20 +21,12 @@ from MulticoreTSNE import MulticoreTSNE as TSNE
 from matplotlib import pyplot as plt
 from collections import OrderedDict
 
-import sys
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--baseline', type=int, help='If true, TSNE for baseline, else for SIFE')
-args = parser.parse_args()
-
-# currently works for adversarial model
-# TODO modify for baseline i3d model on toy jester dataset
 # Modify these
 NUM_ACTIONS = 5
 NUM_SCENES = 2
 NUM_FEATURES = 1024
 BATCH_SIZE = 128
-ADVERSARIAL = True
+IS_BASELINE = False # modify if want to use baseline or not
 DATA_PARALLEL = False # Was model trained using nn.DataParallel?
 CHECKPOINT_PATH = '/vision/u/samkwong/pytorch-i3d/checkpoints-2019-12-9-15-47-16/22170453.pt' # epoch 22
 FEATURES_PATH = None #'/vision/u/samkwong/pytorch-i3d/input_features_sife_epoch22.npy'
@@ -46,87 +38,19 @@ SCENES_SAVE_PATH = 'input_scenes_sife_epoch22'
 TSNE_ACTION_SAVE_PATH = 'tsne_sife_action_jester_epoch22.png'
 TSNE_SCENE_SAVE_PATH = 'tsne_sife_scene_jester_epoch22.png'
 
-# Don't need to modify these
-NUM_WORKERS = 2
-SHUFFLE = False
-PIN_MEMORY = True
-IS_VAL = True # True = don't randomly offset clips (i.e. don't augment dataset)
 
-# Either load features from disk or compute them
-if FEATURES_PATH:
-    inputs_features = np.load(FEATURES_PATH)
-    inputs_actions = np.load(ACTIONS_PATH)
-    inputs_scenes = np.load(SCENES_PATH)
-else:
-    i3d = InceptionI3d(400, in_channels=3)
-
-    if not ADVERSARIAL:
-        model = i3d
-        model.replace_logits(NUM_ACTIONS)
-    else:
-        model = SIFE(backbone=i3d, num_features=NUM_FEATURES, num_actions=NUM_ACTIONS, num_scenes=NUM_SCENES)
-
-    # Load checkpoint
-    print("Loading checkpoint...")
-
-    if DATA_PARALLEL: # If training was run with nn.DataParallel, need extra steps before loading checkpoint
-        print("used nn.DataParallel")
-        state_dict = torch.load(CHECKPOINT_PATH)['model_state_dict'] # baseline weights
-        checkpoint = OrderedDict()
-        for k, v in state_dict.items():
-            name = k[7:] # remove 'module'
-            checkpoint[name] = v
-    else:
-        print('adv weights')
-        checkpoint = torch.load(CHECKPOINT_PATH)['model_state_dict'] # adversarial weights
-
-    model.load_state_dict(checkpoint)
-    print("Loaded.")
-
-    # Transforms
-    SPATIAL_TRANSFORM = Compose([
-        Resize((224, 224)),
-        ToTensor()
-        ])
-
-    # Load dataset
-    d_train = VideoFolder(root="/vision/group/video/scratch/jester/rgb",
-                          csv_file_input="./data/jester/annotations/jester-v1-train-modified.csv",
-                          csv_file_action_labels="./data/jester/annotations/jester-v1-action-labels.csv",
-                          csv_file_scene_labels="./data/jester/annotations/jester-v1-scene-labels.csv",
-                          clip_size=16,
-                          nclips=1,
-                          step_size=1,
-                          is_val=IS_VAL,
-                          transform=SPATIAL_TRANSFORM,
-                          loader=default_loader)
-
-    print('Size of training set = {}'.format(len(d_train)))
-    train_loader = DataLoader(d_train, 
-                              batch_size=BATCH_SIZE,
-                              shuffle=SHUFFLE, 
-                              num_workers=NUM_WORKERS,
-                              pin_memory=PIN_MEMORY)
-
-    # Move model to CPU/GPU
-    if torch.cuda.is_available():
-        device = torch.device('cuda')
-    else:
-        device = torch.device('cpu')
-        print('Using device:', device)
-    model = model.to(device=device) # move model parameters to CPU/GPU
-
+def extract_data(model, test_loader):
     # Extract features
     print('Starting feature extraction with batch size = {}'.format(BATCH_SIZE))
     inputs_features = np.empty((0, NUM_FEATURES)) # to hold all inputs' feature arrays
     inputs_actions = np.empty(0)
     inputs_scenes = np.empty(0)
-    for i, data in enumerate(train_loader):
+    for i, data in enumerate(test_loader):
         print("Extracting features from batch {}".format(i))
         inputs, action_idxs, scene_idxs = data[0], data[1], data[2]
         inputs = inputs.to(device=device, dtype=torch.float32) 
         with torch.no_grad():
-            if not ADVERSARIAL:
+            if IS_BASELINE:
                 features = model.extract_features(inputs)
             else:
                 features = model.backbone.extract_features(inputs)
@@ -145,50 +69,93 @@ else:
     np.save(ACTIONS_SAVE_PATH, inputs_actions)
     np.save(SCENES_SAVE_PATH, inputs_scenes)
 
+def get_test_loader(model):
+    # Transforms
+    SPATIAL_TRANSFORM = Compose([
+        Resize((224, 224)),
+        ToTensor()
+        ])
+    
+    # Load dataset
+    vf = VideoFolder(root="/vision/group/video/scratch/jester/rgb",
+                          csv_file_input="./data/jester/annotations/jester-v1-train-modified.csv",
+                          csv_file_action_labels="./data/jester/annotations/jester-v1-action-labels.csv",
+                          csv_file_scene_labels="./data/jester/annotations/jester-v1-scene-labels.csv",
+                          clip_size=16,
+                          nclips=1,
+                          step_size=1,
+                          is_val=True, # True means don't randomly offset clips (i.e. don't augment dataset)
+                          transform=SPATIAL_TRANSFORM,
+                          loader=default_loader)
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        parser.print_usage()
-        sys.exit(1)
+    print('Size of training set = {}'.format(len(d_train)))
+    test_loader = DataLoader(vf, 
+                              batch_size=BATCH_SIZE,
+                              shuffle=False, 
+                              num_workers=2,
+                              pin_memory=True)
 
+    # Move model to CPU/GPU
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+        print('Using device:', device)
+    model = model.to(device=device) # move model parameters to CPU/GPU
+    return test_loader
+
+# ------------------------------------------------------------
+
+# Either load features from disk or compute them
+if FEATURES_PATH:
+    inputs_features = np.load(FEATURES_PATH)
+    inputs_actions = np.load(ACTIONS_PATH)
+    inputs_scenes = np.load(SCENES_PATH)
+else:
+    i3d = InceptionI3d(400, in_channels=3)
+    if IS_BASELINE:
+        model = i3d
+        model.replace_logits(NUM_ACTIONS)
+    else:
+        model = SIFE(backbone=i3d, num_features=NUM_FEATURES, num_actions=NUM_ACTIONS, num_scenes=NUM_SCENES)
+    
+    print("Loading checkpoint...")
+    if DATA_PARALLEL: # If training was run with nn.DataParallel, need extra steps before loading checkpoint
+        print("Used nn.DataParallel")
+        state_dict = torch.load(CHECKPOINT_PATH)['model_state_dict'] # baseline weights
+        checkpoint = OrderedDict()
+        for k, v in state_dict.items():
+            name = k[7:] # remove 'module'
+            checkpoint[name] = v
+    else:
+        print('Using sife weights')
+        checkpoint = torch.load(CHECKPOINT_PATH)['model_state_dict'] # sife weights
+    model.load_state_dict(checkpoint)
+    print("Loaded")
+
+    test_loader = get_test_loader(model)
+    inputs_features, inputs_actions, inputs_scenes = extract_data(model, test_loader)
+
+    # Calculate TSNE
+    print("Starting TSNE")
+    features_embedded = TSNE(n_jobs=8).fit_transform(inputs_features) # MultiCoreTSNE automatically uses n_components=2
+    print("Finished TSNE")
+    print('feautures_embedded shape = {}'.format(features_embedded.shape))
+
+    # Plot TSNE for action
     action_colors = ['r', 'g', 'b', 'c', 'm'] # create color list with num elements equal to num action labels 
     action_labels = ['swiping-left', 'swiping-right', 'swiping-down', 'swiping-up', 'other']
-    
-    if args.baseline:
-        # TODO modify for baseline feature extraction here
-        # Calculate TSNE
-        print("Starting TSNE")
-        features_embedded = TSNE(n_jobs=8).fit_transform(inputs_features) # MultiCoreTSNE automatically uses n_components=2
-        print("Finished TSNE")
-        print('feautures_embedded shape = {}'.format(features_embedded.shape))
+    for i, c, label in zip(range(NUM_ACTIONS), action_colors, action_labels):
+        plt.scatter(features_embedded[inputs_actions == i, 0], features_embedded[inputs_actions == i, 1], c=c, label=label) 
+    plt.legend()
+    plt.savefig(TSNE_ACTION_SAVE_PATH)
 
-        # Plot TSNE for action
-        for i, c, label in zip(range(NUM_ACTIONS), action_colors, action_labels):
-            plt.scatter(features_embedded[inputs_actions == i, 0], features_embedded[inputs_actions == i, 1], c=c, label=label) 
-        plt.legend()
-        plt.savefig(TSNE_ACTION_SAVE_PATH)
-
-    else:
-        # TODO put rest of my sife tsne script here
-        # Calculate TSNE
-        print("Starting TSNE")
-        features_embedded = TSNE(n_jobs=8).fit_transform(inputs_features) # MultiCoreTSNE automatically uses n_components=2
-        print("Finished TSNE")
-        print('feautures_embedded shape = {}'.format(features_embedded.shape))
-
-        # Plot TSNE for action
-        for i, c, label in zip(range(NUM_ACTIONS), action_colors, action_labels):
-            plt.scatter(features_embedded[inputs_actions == i, 0], features_embedded[inputs_actions == i, 1], c=c, label=label) 
-        plt.legend()
-        plt.savefig(TSNE_ACTION_SAVE_PATH)
-
-        # Plot TSNE for scene
-        scene_colors = ['orange', 'purple'] # create color list with num elements equal to num scene labels
-        scene_labels = ['swiping', 'other']
-        for i, c, label in zip(range(NUM_SCENES), scene_colors, scene_labels):
-            plt.scatter(features_embedded[inputs_scenes == i, 0], features_embedded[inputs_scenes == i, 1], c=c, label=label) 
-        plt.legend()
-        plt.savefig(TSNE_SCENE_SAVE_PATH)
-
+    # Plot TSNE for scene
+    scene_colors = ['orange', 'purple'] # create color list with num elements equal to num scene labels
+    scene_labels = ['swiping', 'other']
+    for i, c, label in zip(range(NUM_SCENES), scene_colors, scene_labels):
+        plt.scatter(features_embedded[inputs_scenes == i, 0], features_embedded[inputs_scenes == i, 1], c=c, label=label) 
+    plt.legend()
+    plt.savefig(TSNE_SCENE_SAVE_PATH)
 
 
